15ba68bc338ec8e7a0a4ee3d403ae8e4ccc259cc
Manuel Sommerhalder <24297512+msommerh@users.noreply.github.com>
2025-04-30 16:40:49 +0200

    Adding dedicated nutritional information about fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,11 +24,13 @@ ingredients_list = st.multiselect(
     max_selections=5)
 
 if ingredients_list:
-
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     
     # st.write(ingredients_string)
 
@@ -44,7 +46,3 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
-
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
